4302992f49104091da4a7a2f790b9c83472746fe
Wilder Arias <wilder.arias95@gmail.com>
2018-12-11 12:50:23 -0500

    save work to resolve problem 53 in the proyect

--- test.rb
+++ test.rb
@@ -1,3 +1,1 @@
-
-hola este este un nuevo archivo en la rama testing.
-modificando el archivo para ver si me deja cambiar de rama si no he tracked el archivo
+realizando mejoras al proyecto en una nueva rama para luego unirla a nuestra rama principal master.
